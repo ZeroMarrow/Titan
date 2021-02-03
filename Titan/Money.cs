ec10c5e1224ee0d927d424d21a232807bbcdd3a3
ИСТОРИН ВЛАД <xloksik3@gmail.com>
2021-02-03 13:45:06 +0200

    Добавьте файлы проекта.

---
+++ Titan/Money.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Titan
+{
+    class Money
+    {
+        
+        public int nominal;
+        public int Nominal
+        {
+            get { return nominal; }
+            set
+            {
+                if (value == 1 || value == 2 || value == 5 || value == 10 || value == 50 || value == 100 || value == 200 || value == 500 || value == 1000 || value == 2000 || value == 5000)
+                    nominal = value;
+            }
+        }
+
+        public double Quantity { get; set; }
+        public double Sum { get; set; }
+    }
+}
---
+++ Titan/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using printer = System.Console;
+namespace Titan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string rub = " Р";
+            Money m = new Money();
+            m.Nominal = 100;
+            Console.WriteLine(m.Nominal + rub);
+            m.Quantity = 20;
+            Console.WriteLine(m.Quantity);
+            m.Sum = (m.Nominal * m.Quantity);
+            Console.WriteLine(m.Sum + rub);
+            Console.Read();
+        }
+    }
+}
